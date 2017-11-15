45c208b09323a126b555753b2595b9319deca6a3
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2017-11-14 21:38:30 -0600

    Remove unnecessary files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { SharedModule } from './shared/shared.module';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    SharedModule
   ],
   declarations: [
     AppComponent
